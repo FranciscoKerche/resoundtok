a4b22130ae60a489a1559855b2c8d5ea7e0bbfc0
FranciscoKerche <chicowkn@gmail.com>
2022-12-19 22:03:49 -0300

    created package to join all functions for tiktok analysis in RESOUND

---
+++ R/imports.R
@@ -0,0 +1,35 @@
+#' Clean data from Zeeswchimer tiktok download
+#'
+#' @description
+#' The function will clean and organize columns extracted from the the web-app zeeschwimer.
+#'
+#'
+#' @param string. Path to data-set.
+#'
+#' @examples
+#'
+#' read_tok('path/to/file')
+#'
+#'
+
+read_tok <- function(.x){
+  result <- .x %>%
+    janitor::clean_names() %>%
+    dplyr::select(item_id, source_platform_url, description = data_desc, create_time = data_create_time, duration = data_video_duration,
+           ratio = data_video_ratio, cover = data_video_cover, origin_cover = data_video_origin_cover,
+           quality = data_video_format, music_id = data_music_id, music_title = data_music_title,
+           music_cover = data_music_cover_medium, music_author = data_music_author_name, original_music = data_music_original,
+           album_music = data_music_album, digg_count = data_stats_digg_count, share_count = data_stats_share_count,
+           comment_count = data_stats_comment_count, play_count = data_stats_play_count) %>%
+    dplyr::mutate(create_time = invoke_map(as.numeric, create_time),
+           author = stringr::str_sub(source_platform_url, nchar("https://www.tiktok.com/"), -1),
+           engagement = digg_count + comment_count + share_count,
+           reach = play_count) |>
+    tidyr::unnest(create_time) |>
+    dplyr::mutate(date = lubridate::as_datetime(create_time)) |>
+    dplyr::mutate(post_url = stringr::str_c(source_platform_url, "/video/", item_id)) |>
+    dplyr::rename(text = description)
+
+  return(result)
+}
+
